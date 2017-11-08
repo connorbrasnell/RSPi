dfdc1160a4043534a4f3b538f0d36d267ac3afbd
Connor Brasnell <cx475@student.bham.ac.uk>
2017-11-08 14:56:11 +0000

    Added database functionality, the total footfall, and footfalls for each period of the day get entered, and the averages for each day of the week, and averages for each period in a certain day are pulled

--- main_screen.py
+++ main_screen.py
@@ -6,14 +6,21 @@ import matplotlib.animation as animation
 from matplotlib import style
 import matplotlib.pyplot as plt
 import time
+import datetime
 import sys
 import MySQLdb as sql
 
 import tkinter as tk
 from tkinter import ttk
 
-db = MySQLdb.connect("localhost","RSPiUser","RSComponents","RSPi" )
+db = sql.connect("localhost","RSPiUser","RSComponents","RSPi" )
 cursor = db.cursor()
+
+currentDay = "Monday"
+currentDate = '1996-02-15'
+dayAverage = [0,0,0,0,0]
+timePeriodAverage = [0,0,0,0,0]
+footfallX = [1,2,3,4,5]
 
 style.use("ggplot")
 
@@ -103,6 +110,94 @@ def animateLight(i):
     lightAxis.annotate(currentY, xy=(0.84, 1.05), xycoords='axes fraction')
     lightAxis.plot(xsL,ysL)
 
+def endOfDayDB():
+
+    global currentDate
+
+    data = open("daysFootfall.txt","r").read()
+    splitData = data.split('\n')
+
+    footfallValues = []
+
+    for eachLine in splitData:
+        if eachLine != '':
+            footfallValues.append(int(eachLine))
+
+    #currentDate = time.strftime('%Y-%m-%d')
+
+    try:
+        cursor.execute("INSERT INTO weekly(day, todaydate, count) VALUES (%s,%s,%s)", (currentDay, currentDate, footfallValues[0]))
+        cursor.execute("INSERT INTO daily(day, todaydate, timeperiod, count) VALUES (%s,%s,%s,%s)", (currentDay, currentDate, 1, footfallValues[1]))
+        cursor.execute("INSERT INTO daily(day, todaydate, timeperiod, count) VALUES (%s,%s,%s,%s)", (currentDay, currentDate, 2, footfallValues[2]))
+        cursor.execute("INSERT INTO daily(day, todaydate, timeperiod, count) VALUES (%s,%s,%s,%s)", (currentDay, currentDate, 3, footfallValues[3]))
+        cursor.execute("INSERT INTO daily(day, todaydate, timeperiod, count) VALUES (%s,%s,%s,%s)", (currentDay, currentDate, 4, footfallValues[4]))
+        cursor.execute("INSERT INTO daily(day, todaydate, timeperiod, count) VALUES (%s,%s,%s,%s)", (currentDay, currentDate, 5, footfallValues[5]))
+
+        db.commit()
+    except Exception as e:
+        print(e)
+        print("Rollback")
+        db.rollback()
+
+def startOfDayDB():
+
+    global currentDate
+    global currentDay
+    global dayAverage
+    global timePeriodAverage
+
+    currentDate = time.strftime('%Y-%m-%d')
+
+    currentDayInt = datetime.datetime.today().weekday()
+    if currentDayInt == 0:
+        currentDay = 'Monday'
+    elif currentDayInt == 1:
+        currentDay = 'Tuesday'
+    elif currentDayInt == 2:
+        currentDay = 'Wednesday'
+    elif currentDayInt == 3:
+        currentDay = 'Thursday'
+    elif currentDayInt == 4:
+        currentDay = 'Friday'
+    elif currentDayInt == 5:
+        currentDay = 'Saturday'
+    elif currentDayInt == 6:
+        currentDay = 'Sunday'
+
+    sql1 = """SELECT CAST(AVG(count) AS UNSIGNED) FROM weekly GROUP BY day"""
+
+    sql2 = """SELECT CAST(AVG(count) AS UNSIGNED) FROM daily WHERE day='%s' GROUP BY timeperiod""" % (currentDay)
+
+    try:
+
+        cursor.execute(sql1)
+        results = cursor.fetchall()
+
+        dayAverage[0] = results[0][0]
+        dayAverage[1] = results[1][0]
+        dayAverage[2] = results[2][0]
+        dayAverage[3] = results[3][0]
+        dayAverage[4] = results[4][0]
+
+        cursor.execute(sql2)
+        results = cursor.fetchall()
+
+        timePeriodAverage[0] = results[0][0]
+        timePeriodAverage[1] = results[1][0]
+        timePeriodAverage[2] = results[2][0]
+        timePeriodAverage[3] = results[3][0]
+        timePeriodAverage[4] = results[4][0]
+
+        db.commit()
+    except Exception as e:
+        print(e)
+        print("Rollback")
+        db.rollback()
+
+    print("Day Average: ", dayAverage)
+    print("Time Period Average: ", timePeriodAverage)
+
+
 class RSPi(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -121,8 +216,20 @@ class RSPi(tk.Tk):
         for row in range(13):
             container.grid_rowconfigure(row, weight=1)
 
-        label = tk.Label(container, text="Live Traffic Map/Videos",bg='skyblue')
-        label.grid(column=0,row=0,rowspan=7,sticky='nesw')
+        topContainer = tk.Frame(container,bg="skyblue")
+        topContainer.grid(column=0,row=0,rowspan=7,sticky='nesw')
+
+        #label = tk.Label(container, text="Live Traffic Map/Videos",bg='skyblue')
+        #label.grid(column=0,row=0,rowspan=7,sticky='nesw')
+
+        label = tk.Label(topContainer, text="Live Traffic Map/Videos",bg='skyblue')
+        label.grid(column=0,row=0)
+
+        retrieveTest = tk.Button(topContainer, text = 'Start of Day', command = startOfDayDB)
+        retrieveTest.grid(column=0,row=1)
+
+        insertTest = tk.Button(topContainer, text = 'End of Day', command = endOfDayDB)
+        insertTest.grid(column=0,row=2)
 
         label = tk.Label(container, text="Today: 43 Customers",bg='orange')
         label.grid(column=0,row=7,rowspan=1,sticky='nesw')
