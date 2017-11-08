d53e2e3df30b1b14739951d89042d93ad4b31099
Connor Brasnell <cx475@student.bham.ac.uk>
2017-11-08 12:07:49 +0000

    Added sections for all the different sections of the display

--- main_screen.py
+++ main_screen.py
@@ -6,14 +6,20 @@ import matplotlib.animation as animation
 from matplotlib import style
 import matplotlib.pyplot as plt
 import time
+import sys
+import MySQLdb as sql
 
 import tkinter as tk
 from tkinter import ttk
 
+db = MySQLdb.connect("localhost","RSPiUser","RSComponents","RSPi" )
+cursor = db.cursor()
+
 style.use("ggplot")
 
 fig = plt.figure()
-plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.4)
+#plt.subplots_adjust(left=0.025, bottom=0.8, right=0.1, top=0.9, wspace=0.2, hspace=0.2)
+plt.subplots_adjust(bottom=0.05, top=0.91, hspace=0.4)
 
 outsideTempAxis = fig.add_subplot(3,1,1)
 insideTempAxis = fig.add_subplot(3,1,2)
@@ -42,9 +48,11 @@ def animateOutside(i):
     currentY = currentY + '\N{DEGREE SIGN}C'
 
     outsideTempAxis.cla()
+
     outsideTempAxis.set_title('Outside Temperature', fontsize=12)
     outsideTempAxis.set_ylabel('Degrees (\N{DEGREE SIGN}C)', fontsize=10)
     outsideTempAxis.annotate(currentY, xy=(0.92, 1.05), xycoords='axes fraction')
+
     outsideTempAxis.plot(xsO,ysO)
 
 def animateInside(i):
@@ -104,54 +112,41 @@ class RSPi(tk.Tk):
         tk.Tk.wm_title(self, "RS Pi")
 
         container = tk.Frame(self)
-        #container.grid(column=0, row=0)
         container.pack(side="top", fill="both", expand = True)
+        # Container has 2 columns and 13 rows
 
-        #container.grid_rowconfigure(0, weight=1)
-        #container.grid_columnconfigure(0, weight=1)
+        for col in range(2):
+            container.grid_columnconfigure(col, weight=1)
 
-        frame = MainPage(container, self)
-        frame.tkraise()
-        frame.grid(row=0, column=0)
+        for row in range(13):
+            container.grid_rowconfigure(row, weight=1)
 
-class MainPage(tk.Frame):
+        label = tk.Label(container, text="Live Traffic Map/Videos",bg='skyblue')
+        label.grid(column=0,row=0,rowspan=7,sticky='nesw')
 
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        label = tk.Label(container, text="Today: 43 Customers",bg='orange')
+        label.grid(column=0,row=7,rowspan=1,sticky='nesw')
 
-        label = tk.Label(self, text="Graph Page!")
-        label.pack(pady=10,padx=10)
+        label = tk.Label(container, text="Footfall Graphs",fg='white',bg='red')
+        label.grid(column=0,row=8,rowspan=5,sticky='nesw')
 
-        label.grid_rowconfigure(0, weight=1)
-        label.grid_columnconfigure(0, weight=1)
+        canvas = FigureCanvasTkAgg(fig, container)
+        canvas.get_tk_widget().grid(column=1,row=0,rowspan=12,sticky='nesw')
 
-        button1 = ttk.Button(self, text="Back to Home")
-        button1.pack()
-
-        label.grid_rowconfigure(1, weight=1)
-        label.grid_columnconfigure(0, weight=1)
-
-        #label = tk.Label(self, text="Graph Page!")
-        #label.grid(row=0, column=0, rowspan=3)
-
-        #namelbl = ttk.Label(tk.Frame, text="Name")
-        #namelbl.grid(column=0, row=0, rowspan=3)
-
-        canvas = FigureCanvasTkAgg(fig, self)
-        canvas.show()
-        #canvas.get_tk_widget().grid(row=0, column=2, rowspan=3)
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        #canvas.get_tk_widget.grid_rowconfigure(0, weight=1)
-        #canvas.get_tk_widget.grid_columnconfigure(1, weight=1)
-
-        #canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
+        label = tk.Label(container, text="Absolute Radio: American Idiot by Green Day",fg='white',bg='black',font='bold')
+        label.grid(column=1,row=12,rowspan=1,sticky='nesw')
 
 display = RSPi()
 
 aniOutside = animation.FuncAnimation(fig, animateOutside, interval=1000)
 aniInside = animation.FuncAnimation(fig, animateInside, interval=1000)
 aniLight = animation.FuncAnimation(fig, animateLight, interval=1000)
+
+def on_close():
+    db.close()
+    display.destroy()
+    sys.exit()
+
+display.protocol("WM_DELETE_WINDOW",  on_close)
 
 display.mainloop()
